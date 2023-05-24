45de6aed0c6e8d41618d4171fe6d832a686fcdfa
analaurafr <laurafernandes@outlook.com>
2023-05-24 01:13:22 -0300

    teste de mais requisitos

---
+++ script.js
@@ -0,0 +1,39 @@
+function getRandomColor() {
+  const colors = ['blueviolet', 'pink', 'gray'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function saveColorPalette() {
+  const colorDivs = document.querySelectorAll('.color');
+  const colorPalette = [];
+  colorDivs.forEach((colorDiv) => {
+    const colorClass = colorDiv.className.split(' ')[1];
+    colorPalette.push(colorClass);
+  });
+  localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
+}
+
+function loadColorPalette() {
+  const savedColorPalette = localStorage.getItem('colorPalette');
+  if (savedColorPalette) {
+    const colorPalette = JSON.parse(savedColorPalette);
+    const colorDivs = document.querySelectorAll('.color');
+    colorDivs.forEach((colorDiv, index) => {
+      const colorClass = colorPalette[index];
+      colorDiv.className = `color ${colorClass}`;
+    });
+  }
+}
+
+document.getElementById('button-random-color').addEventListener('click', () => {
+  const colorDivs = document.querySelectorAll('.color');
+  colorDivs.forEach((colorDiv, index) => {
+    if (index > 0) {
+      const randomColor = getRandomColor();
+      colorDiv.className = `color ${randomColor}`;
+    }
+  });
+  saveColorPalette();
+});
+
+loadColorPalette();
